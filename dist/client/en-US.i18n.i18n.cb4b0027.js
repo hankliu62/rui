1f8d3d3d9138c0189279f16d41dcf81fb23cc49b
liuxiaocong <liu.xiaocong@puscene.com>
2017-12-20 18:12:29 +0800

    build(component): add message and card component on prod env

--- dist/client/en-US.i18n.i18n.cb4b0027.js
+++ dist/client/en-US.i18n.i18n.cb4b0027.js
@@ -1,1 +1,1 @@
-webpackJsonp([31],{323:function(n,e){n.exports={app:{homepage:{content:"Homepage Content"},login:{content:"Login Page Content"},signup:{content:"SignUp Page Content",title:"SIGN UP",newAccount:"Create Account",tip:"同意并接受",userAgreement:"《服务条款》"}}}}});
+webpackJsonp([32],{326:function(n,e){n.exports={app:{homepage:{content:"Homepage Content"},login:{content:"Login Page Content"},signup:{content:"SignUp Page Content",title:"SIGN UP",newAccount:"Create Account",tip:"同意并接受",userAgreement:"《服务条款》"}}}}});
