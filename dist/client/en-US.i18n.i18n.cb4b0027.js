5f2c664d0b175ce3fb87d7d65a40b5186064d301
liuxiaocong <liu.xiaocong@puscene.com>
2017-12-29 17:24:19 +0800

    build(component): refine tabs component on prod env

--- dist/client/en-US.i18n.i18n.cb4b0027.js
+++ dist/client/en-US.i18n.i18n.cb4b0027.js
@@ -1,1 +1,1 @@
-webpackJsonp([34],{334:function(n,e){n.exports={app:{homepage:{content:"Homepage Content"},login:{content:"Login Page Content"},signup:{content:"SignUp Page Content",title:"SIGN UP",newAccount:"Create Account",tip:"同意并接受",userAgreement:"《服务条款》"}}}}});
+webpackJsonp([35],{343:function(n,e){n.exports={app:{homepage:{content:"Homepage Content"},login:{content:"Login Page Content"},signup:{content:"SignUp Page Content",title:"SIGN UP",newAccount:"Create Account",tip:"同意并接受",userAgreement:"《服务条款》"}}}}});
