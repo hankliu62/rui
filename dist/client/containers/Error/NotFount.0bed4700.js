b33aed3fc75e5e3a507d3e2358c1af8bf10aaba4
hankliu <hank.liu@bazhuayuai.com>
2018-01-08 20:48:56 +0800

    feat(chore): remove dist files

--- dist/client/containers/Error/NotFount.0bed4700.js
+++
@@ -1,1 +0,0 @@
-webpackJsonp([22],{303:function(e,t,l){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var n=function(e){return e&&e.__esModule?e:{default:e}}(l(1));t.default=function(e){return n.default.createElement("div",null,n.default.createElement("h3",null,"No match for ",n.default.createElement("code",null,e.location.pathname)))}}});
--- dist/client/en-US.i18n.i18n.cb4b0027.js
+++
@@ -1,1 +0,0 @@
-webpackJsonp([35],{343:function(n,e){n.exports={app:{homepage:{content:"Homepage Content"},login:{content:"Login Page Content"},signup:{content:"SignUp Page Content",title:"SIGN UP",newAccount:"Create Account",tip:"同意并接受",userAgreement:"《服务条款》"}}}}});
